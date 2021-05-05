4db9c937189a4ba9da5d304b6544d6ebbd2eb9bd
Madhura Bhujbal <madhura.bhujbal@and.digital>
2021-05-05 10:26:14 +0100

    Create component to handle errors

--- login-page/src/components/Form.js
+++ login-page/src/components/Form.js
@@ -7,44 +7,51 @@ class Form extends Component {
         this.state = {
             email: '',
             password: '',
-            
+            formErrors: {email: '', password: ''},
+            emailValid: false,
+            passwordValid: false,
+            formValid: false
         }
     }
+
+    
 
     handleUserInput(e) {
         let name = e.target.name;
         let value = e.target.value;
-        this.setState({[name]: value});
+        this.setState({[name]: value}, () => { this.validateField(name, value)});
     }
 
     render() {
         return (
-            <form className="signinForm">
-                <h2>Sign up</h2>
-                <div className="form-group">
-                    <label htmlFor="email">Email address</label>
-                    <input 
-                        type="email" 
-                        className="form-control" 
-                        placeholder="enter email" 
-                        name="email" 
-                        value={this.state.email}
-                        onChange={(event) => this.handleUserInput(event)}
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input 
-                        type="password" 
-                        className="form-control" 
-                        placeholder="enter password" 
-                        name="password" 
-                        value={this.state.password}
-                        onChange={(event) => this.handleUserInput(event)}
-                    />
-                </div>
-                <button type="submit" className="submit-button"> Sign up </button>
-            </form>
+            <div>
+                <form className="signinForm">
+                    <h2>Sign up</h2>
+                    <div className="form-group">
+                        <label htmlFor="email">Email address</label>
+                        <input 
+                            type="email" 
+                            className="form-control" 
+                            placeholder="enter email" 
+                            name="email" 
+                            value={this.state.email}
+                            onChange={(event) => this.handleUserInput(event)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password" 
+                            className="form-control" 
+                            placeholder="enter password" 
+                            name="password" 
+                            value={this.state.password}
+                            onChange={(event) => this.handleUserInput(event)}
+                        />
+                    </div>
+                    <button type="submit" className="submit-button"> Sign up </button>
+                </form>
+            </div>
         )
     }
 }
